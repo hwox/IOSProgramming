d96f56b5c16fa67fa9a162ca70a445377a764184
hwox <v_vpollo@naver.com>
2019-06-16 16:57:33 +0900

    start화면 추가 & UISound 고름
    paper

---
+++ TranditionalHouse_Re/Assets/ExplodeView.swift
@@ -0,0 +1,60 @@
+//
+//  ExplodeView.swift
+//  Yahtzee
+//
+//  Created by KPU_GAME on 27/05/2019.
+//  Copyright © 2019 KPU_GAME. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExplodeView: UIView {
+    
+    private var emitter: CAEmitterLayer!
+    
+    override class var layerClass: AnyClass {
+        return CAEmitterLayer.self
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("use init(frame: ")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        emitter = self.layer as! CAEmitterLayer
+        emitter.emitterPosition = CGPoint(x: self.bounds.size.width / 2, y: self.bounds.size.height / 2)
+        emitter.emitterSize = self.bounds.size
+        
+        emitter.renderMode = CAEmitterLayerRenderMode.additive
+        //emitter.emitterMode = CAEmitterLayerEmitterMode.volume
+        emitter.emitterShape = CAEmitterLayerEmitterShape.rectangle
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        if self.superview == nil {
+            return
+        }
+        let texture: UIImage? = UIImage(named: "particle")
+        assert(texture != nil, "particle image not found")
+        
+        let emitterCell = CAEmitterCell()
+        
+        emitterCell.name = "cell"
+        emitterCell.contents = texture?.cgImage
+        emitterCell.birthRate = 200
+        emitterCell.lifetime = 0.75
+        emitterCell.greenRange = 0.99
+        emitterCell.greenSpeed = -0.99
+        emitterCell.velocity = 160
+        emitterCell.velocityRange = 40
+        emitterCell.scaleRange = 0.5
+        emitterCell.scaleSpeed = -0.2
+        emitterCell.emissionRange = CGFloat(Double.pi * 2)
+        emitter.emitterCells = [emitterCell]
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {self.removeFromSuperview()})
+    }
+}
---
+++ TranditionalHouse_Re/TranditionalHouse/StartTableViewController.swift
@@ -0,0 +1,93 @@
+//
+//  StartTableViewController.swift
+//  TranditionalHouse
+//
+//  Created by KPU_GAME on 16/06/2019.
+//  Copyright © 2019 KPU_GAME. All rights reserved.
+//
+
+import UIKit
+
+class StartTableViewController: UIViewController {
+
+    @IBOutlet weak var MainImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem        UIScreen.main.bounds.width
+   
+    }
+
+    override func viewWillLayoutSubviews() {
+        
+        MainImage.bounds.width
+        MainImage.bounds.height
+        
+        
+        UIScreen.main.bounds.width
+        UIScreen.main.bounds.height
+    }
+    // MARK: - Table view data source
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueToStart" {
+            
+        }
+    }
+    
+
+}
